6ea00c39391c01e979acba5f85dc160d355d3406
Peter Bellstedt <peter.bellstedt@gmx.de>
2023-09-21 15:42:42 +0200

    Bugfixes

---
+++ BAT/run_files/slurm_run-all-equil.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Start equilibration of all poses
+# nr_of_poses is replaced by correct integer via BAT.py
+x=0
+while [ $x -lt NUMBER_OF_POSES ]; do
+cd pose$x
+sbatch SLURM-run
+cd ../
+let x=x+1
+done
+
