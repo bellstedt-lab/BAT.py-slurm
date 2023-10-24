d331b40961581ed3e305d175e8a0b4c2b867ee1c
Peter Bellstedt <peter.bellstedt@gmx.de>
2023-10-24 16:48:16 +0200

    Added Hostname to Results to avoid overwriting

--- slurm_scripts/6_copy_results.bash
+++ slurm_scripts/6_copy_results.bash
@@ -9,18 +9,20 @@ cd ./BAT/fe/
 
 echo ""
 
+host=$(hostname)
+
 for pose in pose*; do
     if [[ -f "./$pose/Results/Results.dat" ]]; then
-        cp "./$pose/Results/Results.dat" "../../results/results_$pose.dat"
-        echo "Results for $pose:"
+        cp "./$pose/Results/Results.dat" "../../results/results_$pose_$host.dat"
+        echo "Results for $pose on $host:"
         echo ""
-        cat "../../results/results_$pose.dat"
+        cat "../../results/results_$pose_$host.dat"
         echo ""
     elif [ -d "./$pose/" ]; then
-        touch "../../results/results_$pose.err"
-        echo "NO results for $pose"
+        touch "../../results/results_$pose_$host.err"
+        echo "NO results for $pose on $host"
     else
        touch "../../results/results.err"
-       echo "Pose directory not found"
+       echo "Pose directory not found on host $host"
     fi
 done
