abf7bfc6d1bcf23b8f64ca96f64f1fdd944a50e5
Peter Bellstedt <peter.bellstedt@gmx.de>
2023-10-26 09:20:08 +0200

    Added SMILES of ligand to Results

--- slurm_scripts/6_copy_results.bash
+++ slurm_scripts/6_copy_results.bash
@@ -14,6 +14,11 @@ host=$(hostname)
 for pose in pose*; do
     if [[ -f "./$pose/Results/Results.dat" ]]; then
         cp "./$pose/Results/Results.dat" "../../results/results_$pose_$host.dat"
+        # extract SMILES representation of ligand and add to Results file
+        smiles=$(cat "../equil/$pose/vac_ligand.pdb" | obabel -ipdb -osmi 2>/dev/null)
+        echo "" >> "../../results/results_$pose_$host.dat"
+        echo "SMILES of Ligand:" >> "../../results/results_$pose_$host.dat"
+        echo "$smiles" >> "../../results/results_$pose_$host.dat"
         echo "Results for $pose on $host:"
         echo ""
         cat "../../results/results_$pose_$host.dat"
