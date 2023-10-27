1379fc6dd897634bb2fe376829f5bf31a349fe24
Peter Bellstedt <peter.bellstedt@gmx.de>
2023-10-27 10:18:06 +0200

    Bugfix Ligand title

--- slurm_scripts/6_copy_results.bash
+++ slurm_scripts/6_copy_results.bash
@@ -14,8 +14,8 @@ host=$(hostname)
 for pose in pose*; do
     if [[ -f "./$pose/Results/Results.dat" ]]; then
         cp "./$pose/Results/Results.dat" "../../results/results_${pose}_${host}.dat"
-        # extract SMILES representation of ligand and add to Results file
-        smiles=$(cat "../equil/$pose/vac_ligand.pdb" | obabel -ipdb -osmi --gen3d --minimize 2>/dev/null)
+        # extract SMILES representation of ligand and add to Results file (maybe add --gen3d --minimize ?)
+        smiles=$(cat "../equil/$pose/vac_ligand.pdb" | obabel -ipdb -osmi -at "Ligand_${pose}_${host}" 2>/dev/null)
         # convert ligand to sdf and save in results dir just be be sure in case it is needed afterwards
         obabel "../equil/$pose/vac_ligand.pdb" -O "../../results/ligand_${pose}_${host}.sdf" 2>/dev/null
         echo "" >> "../../results/results_${pose}_${host}.dat"
